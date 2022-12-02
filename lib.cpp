5821a3b609da2683a3f061fea3f134098ffa8c6f
Gregorio Minetto <117943888+S5679962@users.noreply.github.com>
2022-12-02 22:06:03 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
 
+bool primi(int n) {
+  int div=1, conta=0;
+  
+  while(counta<=1 && div<=n/2) {
+    if(n%div==0) {
+      conta++;
+  }
+          div++;
+  }
+  if (conta==1)
+ {
+    return true;
+     }else{
+       return false;
+     }
+}
